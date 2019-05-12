64892925508e2915d665c4211952c3f95badb964
Gonçalo Ferreira <grfas@iscte-iul.pt>
2019-05-12 19:31:53 +0100

    GUI UITLIZADORES. Interface Investigador

--- ES_GUI/src/Investigador/Inv_Gestao_Cultura.java
+++ ES_GUI/src/Investigador/Inv_Gestao_Cultura.java
@@ -5,10 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class Inv_Gestao_Cultura extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,11 +38,67 @@ public class Inv_Gestao_Cultura extends JFrame {
 	 */
 	public Inv_Gestao_Cultura() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(64, 34, 70, 26);
+		contentPane.add(comboBox);
+		
+		JLabel lblCultura = new JLabel("Cultura");
+		lblCultura.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCultura.setBounds(179, 32, 90, 26);
+		contentPane.add(lblCultura);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(64, 92, 70, 26);
+		contentPane.add(comboBox_1);
+		
+		JLabel lblVariavel = new JLabel("Variavel");
+		lblVariavel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblVariavel.setBounds(179, 93, 70, 20);
+		contentPane.add(lblVariavel);
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnPesquisar.setBounds(309, 88, 97, 31);
+		contentPane.add(btnPesquisar);
+		
+		textField = new JTextField();
+		textField.setBounds(215, 222, 136, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblMedicao = new JLabel("Medicao");
+		lblMedicao.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblMedicao.setBounds(94, 223, 97, 20);
+		contentPane.add(lblMedicao);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(215, 273, 136, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblData = new JLabel("Data");
+		lblData.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblData.setBounds(94, 279, 57, 20);
+		contentPane.add(lblData);
+		
+		JLabel lblaammddHhmmss = new JLabel("(AA-MM-DD HH:MM:SS)");
+		lblaammddHhmmss.setBounds(215, 304, 136, 14);
+		contentPane.add(lblaammddHhmmss);
+		
+		JButton btnInserir = new JButton("Inserir");
+		btnInserir.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnInserir.setBounds(94, 355, 89, 23);
+		contentPane.add(btnInserir);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setBounds(340, 406, 89, 23);
+		contentPane.add(btnSair);
 	}
 
 }
--- ES_GUI/src/Investigador/Inv_Gestao_Cultura_Pesquisa.java
+++ ES_GUI/src/Investigador/Inv_Gestao_Cultura_Pesquisa.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JList;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class Inv_Gestao_Cultura_Pesquisa extends JFrame {
 
@@ -31,11 +36,49 @@ public class Inv_Gestao_Cultura_Pesquisa extends JFrame {
 	 */
 	public Inv_Gestao_Cultura_Pesquisa() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 500);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCultura = new JLabel("Cultura");
+		lblCultura.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCultura.setBounds(77, 46, 46, 14);
+		contentPane.add(lblCultura);
+		
+		JLabel lblnomecultura = new JLabel("_Nome_Cultura_");
+		lblnomecultura.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblnomecultura.setBounds(180, 42, 111, 23);
+		contentPane.add(lblnomecultura);
+		
+		JLabel lblVariavel = new JLabel("Variavel");
+		lblVariavel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblVariavel.setBounds(77, 93, 46, 14);
+		contentPane.add(lblVariavel);
+		
+		JLabel lblnomevariavel = new JLabel("_Nome_Variavel_");
+		lblnomevariavel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblnomevariavel.setBounds(180, 89, 111, 23);
+		contentPane.add(lblnomevariavel);
+		
+		JList list = new JList();
+		list.setBounds(62, 145, 375, 224);
+		contentPane.add(list);
+		
+		JLabel lblMedicoes = new JLabel("Medicoes :");
+		lblMedicoes.setBounds(62, 129, 61, 14);
+		contentPane.add(lblMedicoes);
+		
+		JButton btnApagar = new JButton("Apagar");
+		btnApagar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnApagar.setBounds(43, 380, 89, 23);
+		contentPane.add(btnApagar);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnSair.setBounds(348, 427, 89, 23);
+		contentPane.add(btnSair);
 	}
-
 }
